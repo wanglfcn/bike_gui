f07e58e4aa48c46e3b0eaf9e2d088579cb2a47e4
wanglf <m@wanglf.xyz>
2024-10-19 20:06:57 +0800

    fail

--- bike_gui.ino
+++ bike_gui.ino
@@ -1,6 +1,9 @@
 #include "src/ui/ui.h"
 #include <lvgl.h>
-#include <TFT_eSPI.h>
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
+
 
 static const uint16_t screenWidth  = 240;
 static const uint16_t screenHeight = 320;
@@ -8,32 +11,28 @@ static const uint16_t screenHeight = 320;
 static lv_disp_draw_buf_t draw_buf;
 static lv_color_t buf[ screenWidth * screenHeight / 10 ];
 
-TFT_eSPI tft = TFT_eSPI(screenWidth, screenHeight); /* TFT instance */
 
-/* Display flushing */
-void my_disp_flush( lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p )
-{
-    uint32_t w = ( area->x2 - area->x1 + 1 );
-    uint32_t h = ( area->y2 - area->y1 + 1 );
+#define TFT_CS   10 //34 //     10 or 34 (FSPI CS0) 
+#define TFT_MOSI 11 //35 //     11 or 35 (FSPI D)
+#define TFT_SCLK 12 //36 //     12 or 36 (FSPI CLK)
+#define TFT_MISO 13 //37 //     13 or 37 (FSPI Q)
+// Use pins in range 0-31
+#define TFT_DC    7
+#define TFT_RST   6
 
-    tft.startWrite();
-    tft.setAddrWindow( area->x1, area->y1, w, h );
-    tft.pushColors( ( uint16_t * )&color_p->full, w * h, true );
-    tft.endWrite();
+Adafruit_ST7789 *gfx = new Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
-    lv_disp_flush_ready( disp_drv );
-}
 
 // Display flush function for LVGL
-void my_disp_flush2(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
 {
     int32_t w = (area->x2 - area->x1 + 1);
     int32_t h = (area->y2 - area->y1 + 1);
 
     // Use draw16bitRGBBitmap() to draw the pixel data
-    //gfx->startWrite();
-    //gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)color_p, w, h);
-    //gfx->endWrite();
+    gfx->startWrite();
+    gfx->drawRGBBitmap(area->x1, area->y1, (uint16_t *)color_p, w, h);
+    gfx->endWrite();
 
     lv_disp_flush_ready(disp); // Indicate that flushing is done
 }
@@ -47,33 +46,33 @@ void setup() {
 
     Serial.println( LVGL_Arduino );
     Serial.println( "I am LVGL_Arduino" );
+    
+    gfx->init(240, 320);
+    gfx->fillScreen(ST77XX_BLACK);
+//    gfx->Display_Brightness(255);
+
+    // gfx->setRotation(1);  // Try different rotations: 0, 1, 2, 3
+
+    // Initialize LVGL
+    Serial.println("Initializing LVGL...");
 
     lv_init();
 
-
-    tft.begin();          /* TFT init */
-    //tft.setRotation( 1 ); /* Landscape orientation, flipped */
-
     lv_disp_draw_buf_init( &draw_buf, buf, NULL, screenWidth * screenHeight / 10 );
 
-    /*Initialize the display*/
+     // Initialize display driver
     static lv_disp_drv_t disp_drv;
-    lv_disp_drv_init( &disp_drv );
-    /*Change the following line to your display resolution*/
+    lv_disp_drv_init(&disp_drv);
     disp_drv.hor_res = screenWidth;
     disp_drv.ver_res = screenHeight;
     disp_drv.flush_cb = my_disp_flush;
     disp_drv.draw_buf = &draw_buf;
-    lv_disp_drv_register( &disp_drv );
+    lv_disp_drv_register(&disp_drv);
+    Serial.println("Display driver registered.");
 
-  
-    /* Create simple label */
-    lv_obj_t *label = lv_label_create( lv_scr_act() );
-    lv_label_set_text( label, "Hello Ardino and LVGL!");
-    lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
- 
 
-    delay(2000);
+
+    // Initialize the user interface
       ui_init();
     
     Serial.println( "Setup done" );
